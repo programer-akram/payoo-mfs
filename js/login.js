8e54a6668921d2fa5b7563772c036e796df0bf7f
programer-akram <mah.akram202@gmail.com>
2024-09-22 13:06:05 +0600

    event handler and prevent default add

---
+++ js/login.js
@@ -0,0 +1,26 @@
+
+//step-1: set event handler
+document.getElementById('login-btn').addEventListener('click', function(event){
+   //step-2: prevent default behavior(prevent reloading browser)
+    event.preventDefault();  // vejal to beginers
+    console.log('login button clicked');
+
+    //step-3: get the phone number
+    const phoneNumber = document.getElementById('phone-number').value;
+
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber)
+
+    if(phoneNumber === '01792999202' && pinNumber === '20658'){
+        console.log('successfully login')
+    }else{
+        console.log('phone number or pin is wrong')
+    }
+    
+
+    
+})
+
+
+
+
